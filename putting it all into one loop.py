0a717c538dd31668b70cb2b4acfaa560d51d1deb
Thom Chittom <tchittom@gmail.com>
2023-04-11 15:11:14 -0500

    Add files via upload
    This is my beginning attempt to add all of the projects in chapter four together into one loop. I have commented out the last three or so pseudocode/if statements just to see if I could figure out where the problem is coming from.

---
+++ putting it all into one loop.py
@@ -0,0 +1,42 @@
+scores = [60, 50, 60, 58, 54, 54, 58,
+          50, 52, 54, 48, 69, 34, 55,
+          51, 52, 44, 51, 69, 64, 66,
+          55, 52, 61, 46, 31, 57, 52,
+          44, 18, 41, 53, 55, 61, 51, 44]
+
+
+costs = [.25, .27, .25, .25, .25, .25, .33,
+         .31, .25, .29, .27, .22, .31, .25,
+         .25, .33, .21, .25, .25, .25, .28,
+         .25, .24, .22, .20, .25, .30, .25,
+         .24, .25, .25, .25, .27, .25, .26, .29]
+
+high_score = 0
+length = len(scores)
+cost = 100.0
+most_effective = 0
+
+for i in range(length):
+    #print Bubble solutions
+    print('Bubble solution #' + str(i), 'score:', scores[i])
+    
+    #print number of bubble tests
+    print('Bubbles tests:', length)
+    
+    #print the highest bubble score
+    if scores[i] > high_score:
+        high_score = scores[i]
+    print('Highest bubble score:', high_score)
+
+    #print the solutions with the highest score
+    #if scores[i] == high_score:     
+    #    best_solutions.append(i)
+    #print('Solutions with the highest score:', best_solutions)
+
+    #print the most effective solution and its related cost
+    #index = best_solutions[i]
+    #if cost > costs[index]:
+    #    most_effective = index
+    #    cost = costs[index]
+    #print('Solution', most_effective,
+    #  'is the most effective with a cost of', costs[most_effective])
