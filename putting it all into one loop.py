0794f64b42368e5ee6f5f7db439f067702341317
Thom Chittom <tchittom@gmail.com>
2023-04-12 13:09:20 -0500

    Update putting it all into one loop.py

--- putting it all into one loop.py
+++ putting it all into one loop.py
@@ -11,32 +11,35 @@ costs = [.25, .27, .25, .25, .25, .25, .33,
          .25, .24, .22, .20, .25, .30, .25,
          .24, .25, .25, .25, .27, .25, .26, .29]
 
+best_solutions = []
+
 high_score = 0
 length = len(scores)
 cost = 100.0
 most_effective = 0
+index = best_solutions[i]
 
 for i in range(length):
-    #print Bubble solutions
-    print('Bubble solution #' + str(i), 'score:', scores[i])
-    
-    #print number of bubble tests
-    print('Bubbles tests:', length)
-    
-    #print the highest bubble score
     if scores[i] > high_score:
         high_score = scores[i]
-    print('Highest bubble score:', high_score)
+    if scores[i] == high_score:     
+        best_solutions.append(i)
+    if cost > costs[index]:
+        most_effective = index
+        cost = costs[index]
 
-    #print the solutions with the highest score
-    #if scores[i] == high_score:     
-    #    best_solutions.append(i)
-    #print('Solutions with the highest score:', best_solutions)
+#print Bubble solutions
+print('Bubble solution #' + str(i), 'score:', scores[i])
+   
+#print number of bubble tests
+print('Bubbles tests:', length)
+    
+#print the highest bubble score
+print('Highest bubble score:', high_score)
 
-    #print the most effective solution and its related cost
-    #index = best_solutions[i]
-    #if cost > costs[index]:
-    #    most_effective = index
-    #    cost = costs[index]
-    #print('Solution', most_effective,
-    #  'is the most effective with a cost of', costs[most_effective])
+#print the solutions with the highest score
+print('Solutions with the highest score:', best_solutions)
+
+#print the most effective solution and its related cost
+print('Solution', most_effective,
+      'is the most effective with a cost of', costs[most_effective])
